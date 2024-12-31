6fc40038dd22a3776caacff4da43116e204b199d
Kent Chiu <kent.cwg@gmail.com>
2024-12-31 22:36:39 +0800

    auto reload modify file

--- lua/aider/init.lua
+++ lua/aider/init.lua
@@ -15,4 +15,45 @@ function M.send(text)
   require("aider.aider").send(text)
 end
 
+--- Watch current buffer's file for changes and auto reload
+--- This function sets up a file system watcher that monitors the current buffer's file
+--- for changes. When changes are detected:
+--- 1. Notifies the user
+--- 2. Reloads the file if buffer is not modified
+--- @note Uses vim.uv (libuv) for file system events
+function M.watch_file()
+  local function watch_file()
+    local uv = vim.uv -- Use vim.uv for Neovim 0.10+
+    local filename = vim.fn.expand("%:p")
+    local handle = uv.new_fs_event()
+
+    handle:start(filename, {}, function(err, _, _)
+      if err then
+        vim.api.nvim_err_writeln("Error watching file: " .. err)
+        return
+      end
+
+      -- Schedule UI updates to run on the main thread
+      -- This ensures buffer operations happen safely in Neovim's event loop
+      vim.schedule(function()
+        vim.notify("File has changed:", vim.log.levels.INFO)
+        if vim.bo.modified then
+        else
+          -- Reload the buffer content directly from disk
+          -- This is more efficient than using checktime
+          local bufnr = vim.api.nvim_get_current_buf()
+          vim.api.nvim_buf_call(bufnr, function()
+            vim.cmd("e!")
+          end)
+        end
+      end)
+    end)
+  end
+
+  vim.api.nvim_create_autocmd("BufEnter", {
+    pattern = "*",
+    callback = watch_file,
+  })
+end
+
 return M
--- plugin/aider.lua
+++ plugin/aider.lua
@@ -18,3 +18,54 @@ end, {
   nargs = "*",
   desc = "Run Aider Command",
 })
+
+vim.api.nvim_create_user_command("AiderChat", function(opts)
+  require("aider.chat").open(opts.args)
+end, {
+  nargs = "*",
+  desc = "Open Aider Chat",
+})
+
+vim.api.nvim_create_user_command("AiderTest", function(opts)
+  local chat = require("aider.chat")
+  local buf, win = chat.open(opts.args)
+  chat.insert("Hello")
+end, {
+  nargs = "*",
+  desc = "Aider Test",
+})
+
+vim.api.nvim_create_user_command("AiderInsert", function()
+  local chat = require("aider.chat")
+
+  -- Get visual selection
+  local start_pos = vim.fn.getpos("'<")
+  local end_pos = vim.fn.getpos("'>")
+  local lines = vim.api.nvim_buf_get_lines(0, start_pos[2] - 1, end_pos[2], false)
+
+  -- Get current buffer info
+  local current_buf = vim.api.nvim_get_current_buf()
+  local filename = vim.fn.expand("%:p")
+  local filetype = vim.bo[current_buf].filetype
+
+  -- Format the selection info
+  local info = string.format("%s (lines %d-%d):", filename, start_pos[2], end_pos[2])
+
+  -- Create markdown code block
+  local markdown = {
+    info,
+    string.format("```%s", filetype),
+  }
+
+  -- Add selected lines
+  vim.list_extend(markdown, lines)
+  table.insert(markdown, "```")
+
+  -- Insert into chat
+  chat.insert(markdown)
+end, {
+  range = true,
+  desc = "Insert selection into Aider Chat",
+})
+
+require("aider").watch_file()
